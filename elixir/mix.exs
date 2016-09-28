a86eab16a42ef9c534b77395d417c5b722230ea0
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2016-09-28 16:12:42 +0200

    Add ex_kv as the dependency
    That will be the basis for the tracing

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -14,7 +14,7 @@ defmodule ExTrace.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :kv_server]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,6 @@ defmodule ExTrace.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_kv, git: "https://github.com/mentels/ex_kv.git"}]
   end
 end
