863ab7c9321a6d825d6dd171d9de85478ffccf42
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2016-10-25 00:56:46 +0200

    Parametrise test function and add utils

--- elixir/lib/ex_trace.ex
+++ elixir/lib/ex_trace.ex
@@ -6,14 +6,14 @@ defmodule ExTrace do
     send_and_recv(socket, "GET shopping eggs\r\n")
   end
 
-  def test_kv_server() do
+  def test_kv_server(bucket \\ "alaska") do
     opts = [:binary, packet: :line, active: false]
     {:ok, socket} = :gen_tcp.connect('localhost', 4040, opts)
-    send_and_recv(socket, "GET shopping eggs\r\n")
-    send_and_recv(socket, "CREATE shopping\r\n")
-    send_and_recv(socket, "PUT shopping eggs 3\r\n")
-    send_and_recv(socket, "GET shopping eggs\r\n")
-    send_and_recv(socket, "DELETE shopping eggs\r\n")
+    send_and_recv(socket, "GET #{bucket} bear\r\n")
+    send_and_recv(socket, "CREATE #{bucket}\r\n")
+    send_and_recv(socket, "PUT #{bucket} bear 3\r\n")
+    send_and_recv(socket, "GET #{bucket} bear\r\n")
+    send_and_recv(socket, "DELETE #{bucket} bear\r\n")
     :gen_tcp.close(socket)
   end
 
@@ -21,6 +21,20 @@ defmodule ExTrace do
     case Process.whereis(KVServer.TaskSupervisor) do
       pid when is_pid(pid) -> pid
       _ -> raise "Unable to find KVServer pid"
+    end
+  end
+
+  def kv_bucket_supervisor_pid() do
+    case Process.whereis(KV.Bucket.Supervisor) do
+      pid when is_pid(pid) -> pid
+      _ -> raise "Unable to find KV.Bucket.Supervisor pid"
+    end
+  end
+
+  def kv_bucket_supervisor_pids() do
+    case Process.whereis(KV.Bucket.Supervisor) do
+      pid when is_pid(pid) -> pid
+      _ -> raise "Unable to find KV.Bucket.Supervisor pid"
     end
   end
 
